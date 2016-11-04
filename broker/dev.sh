c7e89924948505e878d81b7a0d71879cebacb86c
Jacob O'Bryant <jacobobryant7@gmail.com>
2016-11-04 15:16:27 -0600

    reorganize things

--- broker/dev.sh
+++ broker/dev.sh
@@ -7,4 +7,4 @@ if ! docker network ls | grep -q $network; then
     docker network create --driver bridge $network
 fi
 docker run --rm -it -v $PWD:/code -p 18181:18181 -p 18183:18183 \
-    --network=$network --net-alias broker cabsbroker
+    --network=$network --net-alias broker --name broker cabsbroker
