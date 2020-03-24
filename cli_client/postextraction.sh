df30c28b8982d3ae7877652580485fbfa3c943b5
AndersenJ <johnathan.andersen@byu.edu>
2020-03-24 11:58:30 -0600

    fixed some issues with permissions

--- cli_client/postextraction.sh
+++ cli_client/postextraction.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-echo "postextraction"
+echo "Files extracted; beginning setup"
 
 # For MacOS
 DESTINATION="/Applications/RGSConnect"
 
 # Create destination folder
-mkdir -p ${DESTINATION}
+mkdir -p "$DESTINATION"
+chmod +rx "$DESTINATION"
 
 # Copy files to destination
 cp rgsconnect.py "$DESTINATION"
+chmod +rx "$DESTINATION/rgsconnect.py"
 cp cert.pem "$DESTINATION"
+chmod +r "$DESTINATION/cert.pem"
 cp CABS_client.conf "$DESTINATION"
+chmod +r "$DESTINATION/CABS_client.conf"
 
 # For MacOS
 cp rgs.sh /usr/local/bin/rgs
+chmod +rx /usr/local/bin/rgs
