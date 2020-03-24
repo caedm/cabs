1a953a009ebaf544cc723b0513d6bd7ed5f6ee5f
AndersenJ <johnathan.andersen@byu.edu>
2020-03-24 11:51:44 -0600

    The CLI client might work on MacOS now. Who knows.

--- cli_client/postextraction.sh
+++ cli_client/postextraction.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
 echo "postextraction"
+
+# For MacOS
+DESTINATION="/Applications/RGSConnect"
+
+# Create destination folder
+mkdir -p ${DESTINATION}
+
+# Copy files to destination
+cp rgsconnect.py "$DESTINATION"
+cp cert.pem "$DESTINATION"
+cp CABS_client.conf "$DESTINATION"
+
+# For MacOS
+cp rgs.sh /usr/local/bin/rgs
